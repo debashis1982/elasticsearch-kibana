76ecb1d4d3eac3209c73a0e03e45e78d3276f3c3
a027780 <debashis.ghosh@target.com>
2017-03-15 20:58:24 -0500

    elastic

---
+++ elasticsearch/template.tf
@@ -0,0 +1,35 @@
+provider "aws" {}
+resource "aws_instance" "lab" {
+    ami = "ami-1e299d7e"
+    instance_type = "t2.micro"
+    tags {
+        Name = "elasticsearch"
+    }
+   key_name = "debashis1982-new"
+    provisioner "file" {
+      source = "etc/yum.repos.d/elasticsearch.repo"
+      destination = "/tmp/elasticsearch.repo"
+      connection {
+            type = "ssh"
+            user = "ec2-user"
+            private_key = "${file("~/.ssh/debashis1982-new.pem")}"
+      }
+    }
+    provisioner "remote-exec" {
+        inline = [
+          "sudo yum -y install java-1.8.0",
+          "sudo yum -y remove java-1.7.0-openjdk",
+          "sudo mv /tmp/elasticsearch.repo /etc/yum.repos.d/elasticsearch.repo",
+	  "sudo yum -y install elasticsearch",
+	  "sudo sed -i 's/\\-Xm/#\\ \\-Xm/g' /etc/elasticsearch/jvm.options",
+	  "sudo sh -c 'echo -Xms500m >>  /etc/elasticsearch/jvm.options'",
+	  "sudo sh -c 'echo -Xmx500m >>  /etc/elasticsearch/jvm.options'",
+	  "sudo service elasticsearch start"
+        ]
+         connection {
+            type = "ssh"
+            user = "ec2-user"
+            private_key = "${file("~/.ssh/debashis1982-new.pem")}"
+         }
+    }
+}
